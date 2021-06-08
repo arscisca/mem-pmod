d07f9d2e1ac0b6c7639cd17c0dbb8bfb907141ce
Alessandro Scisca <s276032@studenti.polito.it>
2021-06-08 10:20:54 +0200

    update tests

--- test/test_Measurements.cpp
+++ test/test_Measurements.cpp
@@ -1,15 +1,15 @@
 #include "test.h"
 #include "Measurements.h"
-
+using namespace std::complex_literals;
 TEST(Measurements, Input) {
     // Read measurements from text file
     Measurements m = readMeasurements(test_data_path + "/measurements.txt");
     ASSERT_EQ(m.frequencies.size(), 50ull) << "Incorrect size of sampled data";
     for (size_t i = 0; i < m.frequencies.size(); i++) {
         ASSERT_DOUBLE_EQ(m.frequencies[i], (i + 1) * 100.0e6) << "Frequency does not have the expected value";
-        ASSERT_COMPLEX_EQ(m.Z11[i], Complex(i + 1) * (1.0 + 1.0i)) << "Z11 does not have the expected value";
-        ASSERT_COMPLEX_EQ(m.Z12[i], Complex(i + 1) * (1.0 + 1.0i)) << "Z12 does not have the expected value";
-        ASSERT_COMPLEX_EQ(m.Z21[i], Complex(i + 1) * (1.0 + 1.0i)) << "Z21 does not have the expected value";
-        ASSERT_COMPLEX_EQ(m.Z22[i], Complex(i + 1) * (1.0 + 1.0i)) << "Z22 does not have the expected value";
+        ASSERT_COMPLEX_EQ(m.Z[i](0, 0), Complex(i + 1) * (1.0 + 1.0i)) << "Z11 does not have the expected value";
+        ASSERT_COMPLEX_EQ(m.Z[i](0, 1), Complex(i + 1) * (1.0 + 1.0i)) << "Z12 does not have the expected value";
+        ASSERT_COMPLEX_EQ(m.Z[i](1, 0), Complex(i + 1) * (1.0 + 1.0i)) << "Z21 does not have the expected value";
+        ASSERT_COMPLEX_EQ(m.Z[i](1, 1), Complex(i + 1) * (1.0 + 1.0i)) << "Z22 does not have the expected value";
     }
 }
--- test/test_TSection.cpp
+++ test/test_TSection.cpp
@@ -8,14 +8,14 @@ TEST(TestTSection, ParameterComputation) {
     TSection section(length);
     PULParameters pul_parameters = BASIC_PARAMETERS;
     constexpr double frequency = 100.0e6;
-    Parameters parameters = section.computeParameters(frequency, pul_parameters);
-    Parameters expected = {
+    section.setParameters(pul_parameters, frequency);
+    LumpedParameters expected = {
             length * pul_parameters.Rsac * sqrt(2 * M_PI * frequency),
             length * pul_parameters.Ls,
             1.0 / length * pul_parameters.Rpac * sqrt(2 * M_PI * frequency),
             1.0 / length * pul_parameters.Lp,
             length * pul_parameters.Cp};
-    ASSERT_EQ(parameters, expected) << "Parameters mismatch";
+    ASSERT_EQ(section.getParameters(), expected) << "Parameters mismatch";
 }
 
 TEST(TestTSection, ABCDMatrix) {
@@ -23,7 +23,8 @@ TEST(TestTSection, ABCDMatrix) {
     TSection section(length);
     PULParameters pul_parameters = BASIC_PARAMETERS;
     double frequency = 100.0e6;
-    Matrix2 m = section.computeABCD(frequency, pul_parameters);
+    section.setParameters(pul_parameters, frequency);
+    Matrix2 m = section.ABCD(frequency);
     // Approximate expected values
     ASSERT_COMPLEX_REL_NEAR(m(0, 0), 1.0, 0.01);
     ASSERT_COMPLEX_REL_NEAR(m(0, 1), 2.5, 0.01);
