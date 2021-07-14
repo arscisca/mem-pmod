4430190eac9e15a80f7d6b71c38321022f1b097a
Alessandro Scisca <s276032@studenti.polito.it>
2021-07-14 11:45:53 +0200

    add plotting script

---
+++ scripts/plot.py
@@ -0,0 +1,64 @@
+import sys
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def _data_properties(fname, title, line):
+    return {
+        'fname': fname,
+        'title': title,
+        'legend': [],
+        'line': line
+    }
+
+
+def _plot_properties(func, label, subplot):
+    return {
+        'func': func,
+        'label': label,
+        'subplot': subplot
+    }
+
+
+def _parse_complex(text):
+    # Remove parentheses
+    text = text[1:-1]
+    real, imag = text.split(",")
+    return complex(float(real), float(imag))
+
+
+def main():
+    measurements = _data_properties(sys.argv[1], 'measured', '--')
+    simulated = _data_properties(sys.argv[2], 'simulated', '-')
+    impedances = {'Z11': 'b', 'Z12': 'r', 'Z22': 'g'}
+    # Plot measurements and then the extracted data
+    for data_properties in [measurements, simulated]:
+        # Read data
+        data = pd.read_table(
+            data_properties['fname'], sep=' ', header=0,
+            converters={
+                'f': float, 'Z11': _parse_complex, 'Z12': _parse_complex, 'Z21': _parse_complex, 'Z22': _parse_complex
+            }
+        )
+        # Plot impedances
+        for plot_properties in [_plot_properties(np.real, 'Re', (2, 1, 1)), _plot_properties(np.imag, 'Im', (2, 1, 2))]:
+            plt.subplot(*plot_properties['subplot'])
+            for i, (impedance, color) in enumerate(impedances.items()):
+                func = plot_properties['func']
+                plt.semilogx(data['f'] / 1e9, func(data[impedance]), f"{data_properties['line']}{color}", zorder=-i)
+                plt.xlabel('f (GHz)')
+                plt.ylabel(f"{plot_properties['label']}(Z) (Ω)")
+                data_properties['legend'].append(f"{impedance} {data_properties['title']}")
+    # Format plots
+    for i, props in enumerate([measurements, simulated]):
+        plt.subplot(2, 1, i + 1)
+        plt.xlim([1e-3, 10])
+        plt.legend(props['legend'], loc='upper right')
+        plt.grid(True, which='both')
+    plt.margins(0.5)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
